e2504ec4e21bead75a6fd71d50f86a197f9fc0cc
Lolbartik89 <vladoleksuk0@gmail.com>
2026-01-16 16:40:26 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ") 
+            todos.append(todo)
+        case "show":
+            for item in todos:
+                print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: ")) -
+            number = number - 1
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo
+            existing_todo = todos[number]
+            print(existing_todo)
+        case "exit":
+            break
+print("Bye!")
