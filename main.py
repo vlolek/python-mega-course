6fc42cdc19d1da21ea92cf175d1ba988a51dbc6e
Lolbartik89 <vladoleksuk0@gmail.com>
2026-01-19 22:49:51 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit:")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a todo: ") 
+            todo = input("Enter a todo: ") + "\n" 
+            file = open("files/todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("files/todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case "show":
+            file = open("files/todos.txt", "r")
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 print (f"{index + 1}-{item.capitalize()}")
                 
+                
+            
         case "edit":
             number = int(input("Number of the todo to edit: ")) 
             number = number - 1
@@ -20,7 +31,7 @@ while True:
         case "complete":
             number = int(input("Number of the todo to complete: ")) 
             number = number - 1
-            todos.pop(number)
+            todos.pop(number - 1)
         case "exit":
             break
 print("Bye!")  
